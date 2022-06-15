ace3791f9e6c613225ec72f7402a3623e8d81309
yasinmillers <ssenyonga100@gmail.com>
2022-06-15 16:26:20 +0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+
+
+        </div>
+
+    </div>
+</div>
