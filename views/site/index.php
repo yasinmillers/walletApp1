bd0cd622ed71a6837fe1d06bb10b3afb3e9cbc71
yasinmillers <ssenyonga100@gmail.com>
2022-06-15 16:49:07 +0300

    new

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Congratulations</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">You have successfully created your Yii.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
