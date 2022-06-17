edec413586758bc27fbc7ae5bd8e51dc0971201d
yasinmillers <ssenyonga100@gmail.com>
2022-06-17 12:09:02 +0300

    testing controllers

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,6 +40,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
