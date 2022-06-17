b9f782ae578c7b4c9e14ee31de0ce42e4391c0ed
yasinmillers <ssenyonga100@gmail.com>
2022-06-17 12:36:51 +0300

    header file

---
+++ views/layouts/header.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+
+NavBar::begin([
+    'brandLabel' => Yii::$app->name,
+
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => [
+        'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+    ],
+]);
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav ml-auto'],
+    'items' => [
+
+        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Contact', 'url' => ['/site/contact']],
+
+        Yii::$app->user->isGuest ? (
+        ['label' => 'Login', 'url' => ['/site/login']]
+        ) : (
+            '<li>'
+            . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+            . Html::submitButton(
+                'Logout (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>'
+        )
+    ],
+]);
+NavBar::end();
