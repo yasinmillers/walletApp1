2ee52f3c440ca96f9b6084b87eb80f07c3c31e8e
yasinmillers <ssenyonga100@gmail.com>
2022-06-18 18:08:41 +0300

    header file change location

--- views/site/index.php
+++ views/site/index.php
@@ -1,25 +1,46 @@
 <?php
 
 /** @var yii\web\View $this */
+/** @var yii\bootstrap4\ActiveForm $form */
+/** @var app\models\Video $model */
+/** @var yii\web\View $this */
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
 
 $this->title = 'YouTube';
 ?>
-<div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations</h1>
+<?=
+    \yii\widgets\ListView::widget([
+        "dataProvider" => $dataProvider,
+        'itemView' => 'single_small_video_card',
+        'options' => ['class' => "row"],
+        'itemOptions' => [
+            'tag' => 'div',
+            'class' => 'col-lg-4, col-md-4, col-sm-6, col-xs-12'
+        ]
+    ])
+?>
 
-        <p class="lead">You have successfully created your Yii.</p>
+<?php $form = ActiveForm::begin([
+    'id' => 'video_form',
+    'layout' => 'horizontal',
+    'fieldConfig' => [
+        'template' => "{label}\n{input}\n{error}",
+        'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+        'inputOptions' => ['class' => 'col-lg-3 form-control'],
+        'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+    ],
+]); ?>
 
-        <p><a class="btn btn-lg btn-success">Get started with Yii</a></p>
+<?= $form->field($model, 'VideoTitle')->textInput(['autofocus' => true, 'required' => true]) ?>
+<?= $form->field($model, 'Description')->textInput(['autofocus' => true, 'required' => true]) ?>
+<div class="form-group">
+    <div class="offset-lg-1 col-lg-11">
+        <label for="formFileLg" class="form-label">Upload Video</label>
+        <input class="form-control form-control-lg" id="formFileLg" type="file" />
     </div>
 
-    <div class="body-content">
-
-        <div class="row">
-
-
-        </div>
-
-    </div>
 </div>
+<?php ActiveForm::end(); ?>
