d78f76694035a5c821bb354fa3ba61f5dc5440c8
yasinmillers <ssenyonga100@gmail.com>
2022-06-20 15:08:33 +0300

    working on video model

--- models/Video.php
+++ models/Video.php
@@ -116,7 +116,7 @@ class Video extends \yii\db\ActiveRecord
     public function save($runValidation = true, $attributeNames = null){
         $isInsert = $this->isNewRecord;
         if ($isInsert) {
-            $this->video_id = Yii::$app->security->generateRandomString(8);
+            $this->video_id = Yii::$app->security->generateRandomString(10);
             $this->title = $this->video->name;
             $this->video_name = $this->video->name;
         }
