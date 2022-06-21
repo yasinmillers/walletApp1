742e01de1b68666e049afccbab313b94f9eac740
yasinmillers <ssenyonga100@gmail.com>
2022-06-21 14:30:45 +0300

    upload

--- views/site/upload.php
+++ views/site/upload.php
@@ -2,10 +2,12 @@
 
    use yii\bootstrap4\ActiveForm;
 ?>
+<div class="upload-icon">
+    <i class="fa fa-upload" aria-hidden="true"></i>
+</div>
 <div class="upload">
     <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
     <?= $form->field($model, 'image')->fileInput() ?>
     <button class="btn btn-success">Submit</button>
     <?php ActiveForm::end() ?>
 </div>
-
