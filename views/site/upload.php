8caf255d3fb07bb3d561413402043a983dc8198a
yasinmillers <ssenyonga100@gmail.com>
2022-06-21 09:07:20 +0300

    upload image

--- views/site/upload.php
+++ views/site/upload.php
@@ -1,1 +1,8 @@
 <?php
+
+   use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
+<?= $form->field($model, 'image')->fileInput() ?>
+    <button>Submit</button>
+<?php ActiveForm::end() ?>
