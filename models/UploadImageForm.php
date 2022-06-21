9085877285958becd7ef7f4513a7acbe29d214bb
yasinmillers <ssenyonga100@gmail.com>
2022-06-21 08:58:43 +0300

    video upload

---
+++ models/UploadImageForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+class UploadImageForm extends Model {
+    public $image;
+    public function rules() {
+        return [
+            [['image'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg, png'],
+        ];
+    }
+    public function upload() {
+        if ($this->validate()) {
+            $this->image->saveAs('../uploads/' . $this->image->baseName . '.' .
+                $this->image->extension);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
