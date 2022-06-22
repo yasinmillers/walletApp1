d63f37b2f1c23c826aa573b5394de89daea42200
yasinmillers <ssenyonga100@gmail.com>
2022-06-22 16:14:02 +0300

    comments

--- views/article/view.php
+++ views/article/view.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+
             'title',
             'body:ntext',
             'created_at',
