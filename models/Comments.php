550291d196b0aaee3e7a78144cde7bec0e76a263
yasinmillers <ssenyonga100@gmail.com>
2022-06-22 15:09:06 +0300

    article

---
+++ models/Comments.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comments".
+ *
+ * @property int $id
+ * @property string|null $body
+ * @property string|null $created_at
+ * @property int|null $created_by
+ *
+ * @property User $createdBy
+ */
+class Comments extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'comments';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_at'], 'safe'],
+            [['created_by'], 'default', 'value' => null],
+            [['created_by'], 'integer'],
+            [['body'], 'string', 'max' => 1000],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'body' => 'Body',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CommentsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CommentsQuery(get_called_class());
+    }
+}
