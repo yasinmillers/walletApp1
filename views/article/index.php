227b20f191ec27b1bc500342f453bae3e771c6bf
yasinmillers <ssenyonga100@gmail.com>
2022-06-23 11:29:07 +0300

    user restriction

--- views/article/index.php
+++ views/article/index.php
@@ -10,17 +10,17 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\ArticleSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Articles';
+$this->title = 'Dev_blog';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="article-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+<?php if(!Yii::$app->user->isGuest):?>
     <p>
         <?= Html::a('Create Article', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
+<?php endif;?>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= \yii\widgets\ListView::widget([
