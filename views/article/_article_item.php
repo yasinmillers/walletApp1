733e2a8c6d94545c3c06a0f07aca4b844c6274b9
yasinmillers <ssenyonga100@gmail.com>
2022-06-23 10:52:19 +0300

    item article

--- views/article/_article_item.php
+++ views/article/_article_item.php
@@ -1,1 +1,16 @@
 <?php
+
+
+use app\models\Article;
+
+?>
+
+<div>
+    <a href="<?php echo \yii\helpers\Url::to(['view', 'id' => $model->id]) ?>">
+        <h3><?php echo \yii\helpers\Html::encode($model->title) ?></h3>
+    </a>
+    <div>
+        <?php echo \yii\helpers\StringHelper::truncateWords(\yii\helpers\Html::encode($model->body), 40) ?>
+    </div>
+    <hr>
+</div>
