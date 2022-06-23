8fd67a3dbed19bb32779eee327bbb435b90a3aa9
yasinmillers <ssenyonga100@gmail.com>
2022-06-23 12:33:11 +0300

    user restriction

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -7,6 +7,7 @@ use app\models\ArticleSearch;
 use yii\behaviors\TimestampBehavior;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
+use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 
 /**
@@ -19,19 +20,26 @@ class ArticleController extends Controller
      */
     public function behaviors()
     {
-        return array_merge(
-            parent::behaviors(),
-            [
-                'verbs' => [
-                    'class' => VerbFilter::className(),
-                    'actions' => [
-                        'delete' => ['POST'],
-
-                    ],
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'actions' => ['create', 'update', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
                 ],
-                TimestampBehavior::className(),
-            ]
-        );
+            ],
+        ];
+
     }
 
     /**
