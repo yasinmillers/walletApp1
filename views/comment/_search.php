c228d2cd24720a2a2a8ca64a4443b6eb8e77d82c
yasinmillers <ssenyonga100@gmail.com>
2022-06-26 10:31:57 +0300

    css on view part

--- views/comment/_search.php
+++ views/comment/_search.php
@@ -8,30 +8,4 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="comment-search">
-
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
-
-    <?= $form->field($model, 'id') ?>
-
-    <?= $form->field($model, 'body') ?>
-
-    <?= $form->field($model, 'created_at') ?>
-
-    <?= $form->field($model, 'created_by') ?>
-
-    <?= $form->field($model, 'article_id') ?>
-
-    <?php // echo $form->field($model, 'pinned') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
