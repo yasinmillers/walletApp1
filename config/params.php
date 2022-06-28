73aba8e3e802fbbd92f27d9bc5e731a613a36909
yasinmillers <ssenyonga100@gmail.com>
2022-06-28 06:44:36 +0300

    register api

--- config/params.php
+++ config/params.php
@@ -1,5 +1,7 @@
 <?php
 
+Yii::setAlias('@articleImgPath','/home/yasin/projects/personal/walletApp/uploads/');
+Yii::setAlias('@articleImgUrl','');
 return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
